fbca81857d1161e3538068d58f7f23348e9d8c42
hannwelkin <hannieyousefabadi@gmail.com>
2026-07-21 12:55:34 +0330

    feat(analysis): Add an analysis section to investigate the cohort's preprocessing results using the metadata and logger info

---
+++ scripts/analysis/cohort_metadata_analysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from hne.core.paths import PREPROCESSED_COHORT
+
+df = pd.read_csv(PREPROCESSED_COHORT / "metadata.csv")
+
+
+
+
+
