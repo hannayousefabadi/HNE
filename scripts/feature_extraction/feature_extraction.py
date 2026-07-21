9621edbb3d2678a91ebbebb705d4d6b4db6f059b
hannwelkin <hannieyousefabadi@gmail.com>
2026-07-21 18:49:59 +0330

    feat(feature-extraction): made the phikon-v2 feature extractor a class so it loads the mode once, also implement using GPU and update the feature_extraction.py accordingly"

--- scripts/feature_extraction/feature_extraction.py
+++ scripts/feature_extraction/feature_extraction.py
@@ -1,2 +1,10 @@
-from hne.feature_extraction import phikon_feature_extraction
+from hne.feature_extraction.phikon_v2 import PhikonExtractor
+from hne.core.paths import TILES, PHIKON_FEATURES
+
+def extract_features():
+    phikon = PhikonExtractor()
+    phikon.extract_patient(patients_dir=TILES, output_dir=PHIKON_FEATURES)
+
+if __name__ == "__main__":
+    extract_features()
 
--- src/hne/feature_extraction/phikon_v2.py
+++ src/hne/feature_extraction/phikon_v2.py
@@ -8,43 +8,56 @@ from pathlib import Path
 
 from hne.core.paths import PHIKON_FEATURES, TILES
 
-def phikon_feature_extraction(patients_dir: str = TILES,
-                              output_dir: str = PHIKON_FEATURES):
-    """
-    Extracting tiles features using Phikon-v2 foundation model
-    """
-    processor = AutoImageProcessor.from_pretrained("owkin/phikon-v2")
-    model = AutoModel.from_pretrained("owkin/phikon-v2")
-    model.eval()
+class PhikonExtractor:
+    def __init__(self):
+        """
+        To initialize the model once
+        """
+        # to detect GPU
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"\nLoading Phikon-v2 on {self.device}...")
 
-    output_dir = Path(output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
+        self.processor = AutoImageProcessor.from_pretrained("owkin/phikon-v2")
+        self.model = AutoModel.from_pretrained("owkin/phikon-v2")
+        self.model.to(self.device) # move model to GPU/CPU
+        self.model.eval()
 
-    patients_dir = Path(patients_dir) 
-    patient_dirs = [d for d in patients_dir.iterdir() if d.is_dir()]
-    
-    for patient_dir in patient_dirs:
-        patient_name = patient_dir.name
+    def extract_patient(self, patients_dir: str = TILES,
+                                output_dir: str = PHIKON_FEATURES):
+        """
+        Extracting tiles features using Phikon-v2 foundation model
+        """
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        patients_dir = Path(patients_dir) 
+        patient_dirs = [d for d in patients_dir.iterdir() if d.is_dir()]
         
-        # get all the tiles for this one patient
-        tile_paths = sorted(patient_dir.glob("*.png"))
-        if not tile_paths:
-            print(f"No tile images found for {patient_name}")
-            continue
-        # process each tile        
-        for i, tile_path in enumerate(tile_paths):
-            image = [Image.open(tile_paths).convert("RGB")]
-            # process img
-            input = processor(image, return_tensors="pt")
-            # get the features
-            with torch.inference_mode():
-                output = model(**input)
-                features = output.last_hidden_state[:, 0, :] # shape: (1, 1024)
+        for patient_dir in patient_dirs:
+            patient_name = patient_dir.name
+            
+            # get all the tiles for this one patient
+            tile_paths = sorted(patient_dir.glob("*.png"))
+            if not tile_paths:
+                print(f"No tile images found for {patient_name}")
+                continue
+            # process each tile        
+            for tile_path in tile_paths:
+                image = [Image.open(tile_path).convert("RGB")]
+                # process img
+                inputs = self.processor(image, return_tensors="pt")
+                # move inputs to the same device as the model
+                inputs = {k: v.to(self.device) for k, v in inputs.items()}
+                # get the features
+                with torch.inference_mode():
+                    output = self.model(**inputs)
+                    features = output.last_hidden_state[:, 0, :] # shape: (1, 1024)
 
-            assert features.shape == (1, 1024)
+                assert features.shape == (1, 1024)
 
-            tile_filename = tile_path.stem
-            output_file = output_dir / f"{tile_filename}_phikon_features.npy"
-            np.save(output_file, features.numpy())
+                tile_filename = tile_path.stem
+                output_file = output_dir / f"{tile_filename}_phikon_features.npy"
+                # move items back to CPU before saving as np
+                np.save(output_file, features.cpu().numpy())
 
-    print("\nFeature extraction with Phikon-v2 completed!")        
+        print("\nFeature extraction with Phikon-v2 completed!")        
